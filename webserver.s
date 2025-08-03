d9d28a702081543ea5ef286cf03748793c46b215
Marvin Lihan <m.lihan@campus.tu-berlin.de>
2025-08-03 12:45:46 +0200

    build socket

--- webserver.s
+++ webserver.s
@@ -1,8 +1,14 @@
-.intel_syntax noprefix			# use intel_syntax -> no % or & prafix needed 
+.intel_syntax noprefix			# use intel_syntax -> no % or & prafix needed
 .globl _start				# set the linker the starting point (feels like main in other programming languages)
-.section .text				# text section where the code lays 
+.section .text				# define code section
 _start:
-	mov rax, 60			# syscall for exit 
+	mov rax, 41			# syscall number for the socket syscall
+	mov rdi, 2			# domain = AF_INEt for IPv4 adresses
+	mov rsi, 1			# type = SOCK_STREAm for data transfer with tcp
+	mov rdx, 0			# protocol = 0 (tcp)
+	syscall
+
+	mov rax, 60			# syscall for exit
 	mov rdi, 0
 	syscall
 
